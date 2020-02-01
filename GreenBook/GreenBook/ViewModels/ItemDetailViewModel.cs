00f832e09247665605d079f7cd092c28dd5ca63e
Jesse Baker <jjhb94@gmail.com>
2020-01-31 17:49:19 -0800

    First Push! Let's start building

---
+++ GreenBook/GreenBook/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GreenBook.Models;
+
+namespace GreenBook.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
