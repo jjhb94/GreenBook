164a7998b2054f12d5589a1fbd41bf64f892faf3
Berjak <jjhb94@gmail.com>
2020-02-12 18:41:20 -0600

    Removed unnecessary views pages; Fixed view for smaller screens; Adjusted experimental features so project would build

--- GreenBook/GreenBook.Android/MainActivity.cs
+++ GreenBook/GreenBook.Android/MainActivity.cs
@@ -13,16 +13,17 @@ namespace GreenBook.Droid
     [Activity(Label = "GreenBook", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+    
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-            Forms.SetFlags("CollectionView_Experimental");
+            global::Xamarin.Forms.Forms.SetFlags(new[] {"CollectionView_Experimental", "Shell_Experimental" });
+            Forms.Init(this, savedInstanceState);
 
+            LoadApplication(new App());
         }
     }
 }
